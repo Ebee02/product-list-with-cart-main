a2b53a94a649bc3714f3d39febf3d80f2730f12f
Ebee02 <beelawrence8@gmail.com>
2025-01-01 09:40:19 +0000

    Created the DessertItem component

--- src/components/DessertItem.tsx
+++ src/components/DessertItem.tsx
@@ -2,13 +2,21 @@ import { MdOutlineAddShoppingCart } from "react-icons/md";
 const DessertItem = () => {
   return (
     <div>
-      <div className="border-2 border-black rounded-lg h-52 w-full">
-        <button className="bg-white font-medium rounded-full py-2 px-6 border border-custom-rose-400 text-custom-rose-900 text-lg">
-          <span>
-            <MdOutlineAddShoppingCart />
+      <div className="relative border-2 border-black rounded-lg h-52 w-full">
+        <button className="absolute -bottom-[27px] left-1/2 -translate-x-1/2 flex items-center gap-3 bg-white font-semibold rounded-full py-3 px-4 border border-custom-rose-400 shadow-sm text-lg text-custom-rose-900 cursor-pointer w-[170px]">
+          <span className="text-custom-rose-500">
+            <MdOutlineAddShoppingCart className="text-2xl" />
           </span>{" "}
-          Add to cart
+          <p> Add to Cart</p>
         </button>
+      </div>
+
+      <div className="mt-7">
+        <p className=" text-custom-rose-500">Waffle</p>
+        <p className="text-lg font-semibold text-custom-rose-900">
+          Waffle with Berries
+        </p>
+        <p className="text-primary font-medium text-lg">$6.50</p>
       </div>
     </div>
   );
