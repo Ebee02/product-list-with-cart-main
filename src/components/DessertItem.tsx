ef59f0665bddc2f4983e95dff0b112f0d5348afe
Ebee02 <beelawrence8@gmail.com>
2025-01-07 06:07:08 +0000

    update

--- src/components/DessertItem.tsx
+++ src/components/DessertItem.tsx
@@ -1,5 +1,6 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import { MdOutlineAddShoppingCart } from "react-icons/md";
+import SelectedItemButton from "./SelectedItemButton";
 
 type DessertItemDataType = {
   image: {
@@ -15,6 +16,12 @@ type DessertItemDataType = {
 };
 
 const DessertItem: FC<DessertItemDataType> = (props) => {
+  const [isCartSelected, setIsCartSelected] = useState(false);
+
+  const handleClick = () => {
+    setIsCartSelected(true);
+  };
+
   return (
     <div>
       <div className="relative border-none rounded-lg h-[220px] shadow-md w-full">
@@ -23,12 +30,20 @@ const DessertItem: FC<DessertItemDataType> = (props) => {
           alt="lorem"
           className="h-full w-full object-cover rounded-lg"
         />
-        <button className="absolute -bottom-[25px] left-1/2 -translate-x-1/2 flex items-center gap-3 bg-white font-semibold rounded-full py-3 px-4 border border-custom-rose-400 shadow-sm text-sm text-custom-rose-900 cursor-pointer w-[140px]">
-          <span className="text-custom-rose-500">
-            <MdOutlineAddShoppingCart className="text-lg" />
-          </span>{" "}
-          <p> Add to Cart</p>
-        </button>
+
+        {isCartSelected ? (
+          <SelectedItemButton />
+        ) : (
+          <button
+            onClick={handleClick}
+            className="absolute -bottom-[25px] left-1/2 -translate-x-1/2 flex items-center gap-3 bg-white font-semibold rounded-full py-3 px-4 border border-custom-rose-400 shadow-sm text-sm text-custom-rose-900 cursor-pointer w-[140px]"
+          >
+            <span className="text-custom-rose-500">
+              <MdOutlineAddShoppingCart className="text-lg" />
+            </span>
+            <p> Add to Cart</p>
+          </button>
+        )}
       </div>
 
       <div className="mt-7">
---
+++ src/components/SelectedItemButton.tsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import { CiCirclePlus, CiCircleMinus } from "react-icons/ci";
+
+const SelectedItemButton = () => {
+  const [count, setCount] = useState(0);
+
+  function incrementAmount() {
+    setCount((prevCount) => prevCount + 1);
+  }
+
+  function decrementAmount() {
+    if (count > 0) {
+      setCount((prevCount) => prevCount - 1);
+    }
+  }
+  return (
+    <div className="duration-300 transition-all ease-in-out absolute -bottom-[25px] left-1/2 -translate-x-1/2 text-white flex items-center justify-between gap-3 bg-primary font-semibold rounded-full py-2 px-4 border border-custom-rose-400 shadow-sm text-sm cursor-pointer w-[140px]">
+      <button onClick={decrementAmount} className="text-xl">
+        <CiCircleMinus />
+      </button>
+      <h3 className="text-lg font-semibold">{count}</h3>
+      <button onClick={incrementAmount} className="text-xl">
+        <CiCirclePlus />
+      </button>
+    </div>
+  );
+};
+
+export default SelectedItemButton;
